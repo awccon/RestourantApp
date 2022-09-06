213bf2cf404d26c019f24f0d1671de2f50903c00
Akmal <akamolov@eaglebusinesssoftware.com>
2022-09-06 16:58:38 -0400

    Adding new project "Improving current application"

---
+++ RestaurantApp2/Classes/ChickenOrder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestourantApp.Classes
+{
+    /// <summary>
+    /// This class is to create Chicken order
+    /// </summary>
+    internal class ChickenOrder
+    {
+        private int _quantity = 0;
+
+        /// <summary>
+        /// Chicken order it will return quantity of chicken
+        /// </summary>
+        /// <param name="quantity">You shoudl pass the quantity of chickens</param>
+        public ChickenOrder(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        /// <summary>
+        /// Gets quantity from constructor
+        /// </summary>
+        /// <returns>quantity type integer</returns>
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+
+        /// <summary>
+        /// Method to cut a meat, it shoul be called the num of times in quantity
+        /// </summary>
+        public void CutUp()
+        {
+
+        }
+
+        /// <summary>
+        /// Method to cook a chicken
+        /// </summary>
+        public void Cook()
+        {
+
+        }
+    }
+}
---
+++ RestaurantApp2/Classes/EggOrder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestourantApp.Classes
+{
+    /// <summary>
+    /// This class is to create an Egg order
+    /// </summary>
+    internal class EggOrder
+    {
+        private int _quantity = 0;
+        private static int _eggQualityCount = 0;
+        private int random = new Random().Next(101);
+
+        public EggOrder(int quantity)
+        {
+            _quantity = quantity;
+            _eggQualityCount++;
+        }
+
+        /// <summary>
+        /// Get Quantity of Egg
+        /// </summary>
+        /// <returns>quantity</returns>
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+
+        //1: 55, 2: null, 3: 45, 4: null, 
+
+        /// <summary>
+        /// Get quality method
+        /// </summary>
+        /// <returns>returns a quality of egg with random number</returns>
+        public int? GetQuality()
+        {
+            //CR: quality of the egg should be specific to an instance. It shouldn't renew ever =y time I call the method. It should be different when I create a new EggOrder class.
+            if (_eggQualityCount % 2 == 0) { return null; }
+            else return random;
+        }
+
+        /// <summary>
+        /// Crack method
+        /// </summary>
+        /// <exception cref="Exception">Checks egg quality and throws an exception</exception>
+        public void Crack()
+        {
+            if (GetQuality() <= 25)
+            {
+                throw new Exception("The Egg is rotten");
+            }
+        }
+
+        /// <summary>
+        /// Discard Shell method doesn't return anithing
+        /// </summary>
+        public void DiscardShell()
+        {
+
+        }
+
+        /// <summary>
+        /// Cook method doesn't return anithing
+        /// </summary>
+        public void Cook()
+        {
+
+        }
+    }
+}
