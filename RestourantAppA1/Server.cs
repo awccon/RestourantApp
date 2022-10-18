8f8f954a935b97887cb134263e19e5127eb53aa1
Akmal <akamolov@eaglebusinesssoftware.com>
2022-10-18 09:45:13 -0400

    create new project

---
+++ RestourantAppA1/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestourantAppA1
+{
+	internal class Server
+	{
+		public void SubmitNewRequest()
+		{
+
+		}
+
+		public void CopyPreviousRequest()
+		{
+
+		}
+
+		public void PrepareFood()
+		{
+
+		}
+	}
+}
